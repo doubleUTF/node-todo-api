788996e9302986496c81895354814ac452d8a396
BigD <dlau373@gmail.com>
2017-01-10 11:24:07 -0800

    Edited the mongodb URL with mLab URL

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://genericUser:hahaha@ds161038.mlab.com:61038/todos')
 module.exports={mongoose}
