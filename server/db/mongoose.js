2f4ac0a16ae9aa0afad8207d6555043e36888ebd
BigD <dlau373@gmail.com>
2017-01-10 11:44:42 -0800

    Edited mongoose connect string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ const mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://genericUser:hahaha@ds161038.mlab.com:61038/todos')
+mongoose.connect(process.env.PORT ? 'mongodb://genericUser:hahaha@ds161038.mlab.com:61038/todos': 'mongodb://localhost:27017/TodoApp')
 module.exports={mongoose}
