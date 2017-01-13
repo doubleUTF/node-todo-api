4d4574720c961e65beb780b2a97226b3220d8b7a
BigD <dlau373@gmail.com>
2017-01-13 14:34:10 -0800

    Added POST /users route and JWT

--- server/server.js
+++ server/server.js
@@ -91,6 +91,20 @@ app.patch('/todos/:id', (req,res)=>{
   })
 })
 
+  //POST /users
+app.post('/users',(req,res)=>{
+  var body=_.pick(req.body,['email','password'])
+  var user=new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  },(err)=>{
+    res.status(400).send(err)
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  })
+})
+
 app.listen(port,()=>{
   console.log(`Started on port ${port}`);
 });
